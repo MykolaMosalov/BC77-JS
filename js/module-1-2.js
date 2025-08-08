f26b7dcc141351c219c93b05c858f6d9afb7325f
Nunam-UK <ivanolef.io@gmail.com>
2025-08-08 17:34:02 +0300

    Tusk 4

--- js/module-1-2.js
+++ js/module-1-2.js
@@ -27,8 +27,8 @@ function findSmallNumber(a, b, c, d, e) {
 // console.log(findSmallNumber(12, 7, 3, 8, 9));
 
 // ? Напиши функцію, яка буде перевіряти пароль у користувача. Запроси пароль у користувача через promt
-const userPassword = prompt("Enter your password");
-const userPassword2 = prompt("Enter your password");
+// const userPassword = prompt("Enter your password");
+// const userPassword2 = prompt("Enter your password");
 
 function passwordVerification(password) {
   const origenalPasseord = "admin";
@@ -39,3 +39,16 @@ function passwordVerification(password) {
 // console.log(passwordVerification(userPassword2));
 // console.log(passwordVerification("test"));
 // console.log(passwordVerification("12345"));
+
+// ? Напишіть функцію яка приймає наступні параменти userName з ім’ям користувача і payment з сумою до оплати. Функція повинна допомогою шаблонного рядка вивести через alert повідомлення: «Дякуємо, Олександро! До сплати 300 гривень».
+
+const userName = prompt ("Enter your name")
+const price = prompt ("Paste a price")
+
+function showUserMessage (name, payment) {
+  
+alert(`Дякуємо, ${name}! До сплати ${payment} гривень`)
+}
+
+console.log(showUserMessage(userName, price));
+// console.log(showUserMessage("Микола", 500));
