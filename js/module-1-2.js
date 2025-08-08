73a5dc06601373f011b3e551d985155cecc73119
Oleksandr Shtyka <66411717+OleksandrShtyka@users.noreply.github.com>
2025-08-08 14:59:44 +0300

    create random generator

---
+++ js/module-1-2.js
@@ -0,0 +1,19 @@
+// * Функціі (аргументи параметри, та повернення значення)
+
+/**
+ * Function declaration
+ * function fn(){}
+ *
+ * Function expression
+ * const fn1 = function(){}
+ */
+
+function getRandomNumber(min, max) {
+  // return Math.floor(Math.random() * max + min )
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//Напиши функцію яка буде генерувати рандомне число від 1 - 20
+console.log(getRandomNumber(40, 45));
+// console.log(Math.floor(Math.random() * 21))
+// console.log(Math.floor(Math.random()*(max - min + 1) + min));
