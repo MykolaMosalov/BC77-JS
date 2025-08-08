3dc3924a3dd6c4839e00c2f178a7585fd444568e
helen-akateva <akareva17@gmail.com>
2025-08-08 15:43:32 +0300

    task2

--- js/module-1-2.js
+++ js/module-1-2.js
@@ -17,3 +17,11 @@ function getRandomNumber(min, max) {
 // console.log(getRandomNumber(40, 45));
 // console.log(Math.floor(Math.random() * 21))
 // console.log(Math.floor(Math.random()*(max - min + 1) + min));
+
+// ? Напиши функцію, яка буде виводити найменьше число
+
+function findSmallNumber(a, b, c, d, e) {
+  return Math.min(a, b, c, d, e)
+}
+// console.log(findSmallNumber(10, 8, 2, 4, 9));
+// console.log(findSmallNumber(12, 7, 3, 8, 9));
