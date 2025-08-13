e3fc214926a81b866a3f389b4bb26a1009dfa4d4
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2025-08-13 18:16:32 +0300

    task 5

--- js/module-3-1.js
+++ js/module-3-1.js
@@ -2,8 +2,8 @@
 //Перетворити рядок, що містить слова, розділені комами, в масив слів і вивести кожне слово в окремому рядку
 // за допомогою циклу for і for..of.const str = 'JavaScript, HTML, CSS, React';
 
-const str = "JavaScript, HTML, CSS, React";
-const strArray = str.split(",");
+// const str = 'JavaScript, HTML, CSS, React';
+// const strArray = str.split(',');
 // console.log(strArray);
 
 // for (let i = 0; i < strArray.length; i += 1){
@@ -13,23 +13,24 @@ const strArray = str.split(",");
 // for (const word of strArray) {
 //   console.log(word);
 // }
+
 //TODO:=========task-2=================
 // Порахуй кількість слів у рядку, використовуючи методи масивів і створи новий рядок, роділений дефісами у нижньому регістрі.
 
-const message = "JavaScript is a popular programming language.";
+// const message = 'JavaScript is a popular programming language.';
 
-const res = message.toLowerCase().split(" ").join("-");
+// const res = message.toLowerCase().split(' ').join('-');
 // console.log(res);
-const wordCount = message.split(" ").length;
+// const wordCount = message.split(' ').length;
 // console.log(wordCount);
 
 //TODO:=========task-3=================
 // Знайти всі елементи масиву, що містять задану підстроку, та об'єднати їх у новий масив, після чого вивести у консоль лог повідомлення "Масив елементів, що містять підстроку substring: ..." Результат повинен бути розділений комами та пробілами.(використати indexOf)
 
-const wordsArray = ["JavaScript", "HTML", "CSS", "React"];
-const substring = "S";
+// const wordsArray = ['JavaScript', 'HTML', 'CSS', 'React'];
+// const substring = 'S';
 // console.log("J-0 a-1 v-2 a-3 S-4 cript".indexOf("W"));
-const filteredArray = [];
+// const filteredArray = [];
 
 // for (const element of wordsArray) {
 //   if (element.includes(substring)) {
@@ -61,20 +62,46 @@ const filteredArray = [];
 //     filteredArray.join(", ") || "Відсутня"
 //   }`
 // );
+
 //TODO:=========task-4=================
 // Створити масив з 10-ти чисел. Напиши функцію яуа буде шукати найбільше число.
 
-function findMaxNumber(arr) {
-  let maxNumber = arr[0];
-  for (const number of arr) {
-    if (number > maxNumber) {
-      maxNumber = number;
-    }
-  }
-  return maxNumber;
-}
+// const numbersArray = [1, 0, 5, 46, 23, 15, 4, 800, 7];
+// const numbersArray1 = [10, 15, 40, 350, 500];
+// const numbersArray2 = [25, 35, 1000, 87, 12];
 
-console.log(findMaxNumber(numbersArray));
+// function findMaxNumber(arr) {
+//   let maxNumber = arr[0];
 
-const numbersArray = [1, 0, 5, 46, 23, 15, 4, 800, 7];
-findMaxNumber(numbersArray);
+//   for (const number of arr) {
+//     if (number > maxNumber) {
+//       maxNumber = number;
+//     }
+//   }
+
+//   return maxNumber;
+// }
+
+//console.log(findMaxNumber([1, 0, 5, 46, 23, 15, 4, 800, 7])); // 800
+//console.log(findMaxNumber(numbersArray1)); // 500
+//console.log(findMaxNumber([25, 35, 1000, 87, 12])); // 1000
+
+// const userName = 'Mukola';
+
+// function greed(userName) {
+//   return `Hello ${userName}`;
+// }
+
+// console.log(greed(userName));
+
+// function foo(arr) {
+//   let maxNumber = arr[0];
+
+//   for (const number of arr) {
+//     if (number > maxNumber) {
+//       maxNumber = number;
+//     }
+//   }
+
+//   return maxNumber;
+// }
