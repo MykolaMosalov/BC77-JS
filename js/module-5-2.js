e740275d59519ca6965dbc1fa2a26c9be41093d5
Arthur <shikimoriarti@gmail.com>
2025-08-22 15:32:35 +0300

    fix

--- js/module-5-2.js
+++ js/module-5-2.js
@@ -17,3 +17,18 @@
 // const res = data.flatMap((element) => element.values);
 // console.log(res);
 // console.log(res);
+
+//TODO:=========task-03=================
+// Дано масив об'єктів {name: "John", age: 27}, {name: "Jane", age: 31}, {name: "Bob", age: 19}. Перевірте, чи є хоча б один об'єкт з віком менше 20 років. Очікуваний результат: true.
+
+const people = [
+  { name: "John", age: 11 },
+  { name: "Jane", age: 15 },
+  { name: "Bob", age: 19 },
+];
+
+// const res = people.some((el, indx, array) => el.age < 20);
+// console.log(res);
+
+const res = people.every((el, indx, array) => el.age < 20);
+console.log(res);
