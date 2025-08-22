53a1649833f2052dbb3fbcd80533cbf9a8bc4b0d
Anton Shuvalov <anshuvalov83@gmail.com>
2025-08-22 15:10:44 +0300

    task 2

--- js/module-5-2.js
+++ js/module-5-2.js
@@ -8,11 +8,11 @@
 //TODO:=========task-02=================
 // Дано масив об'єктів {id: 1, values: [1, 2, 3]}, {id: 2, values: [4, 5, 6]}, {id: 3, values: [7, 8, 9]}. Створіть новий масив, що містить всі значення з масивів values кожного об'єкту, збережених в одному масиві. Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
 
-const data = [
-  { id: 1, values: [1, 2, 3] },
-  { id: 2, values: [4, 5, 6] },
-  { id: 3, values: [7, 8, 9] },
-];
+// const data = [
+//   { id: 1, values: [1, 2, 3] },
+//   { id: 2, values: [4, 5, 6] },
+//   { id: 3, values: [7, 8, 9] },
+// ];
 
-const res = data.flatMap((element) => element.values);
+// const res = data.flatMap((element) => element.values);
 // console.log(res);
